104aa747987bd3c2e8a0e2aeef095f7617e0ec36
connerng <cjngadisastra@gmail.com>
2024-06-17 21:20:23 -0700

    started on UI

--- chordprog.py
+++ chordprog.py
@@ -10,17 +10,25 @@ class ChordProgression:
         cur = ""
         self.chordProg = []
         for c in cpStr:
-            if c.isalpha():
+            if c.isalpha() or c == '#':
                 cur += c
             else:
                 if len(cur) > 0:
                     self.chordProg.append(cur)
                 cur = ""
                 self.chordProg.append(c)
+        if len(cur) > 0:
+            self.chordProg.append(cur)
         self.key = key
         self.timeSig = timeSig
 
     def __str__(self):
+        retVal = self.chordProg[0]
+        for i in range(1, len(self.chordProg)):
+            retVal += " " + self.chordProg[i]
+        return retVal
+    
+    def ToString(self):
         retVal = self.chordProg[0]
         for i in range(1, len(self.chordProg)):
             retVal += " " + self.chordProg[i]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from transposer import *
-from tkinter import *
-from tkinter.ttk import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
 from chordprog import *
 
 # test_cp_str = "|C...|Am...|G...|F...|"
@@ -20,16 +21,59 @@ from chordprog import *
 # print(cp3.key)
 # print(cp3)
 
-root = Tk()
+chords = ['.','Ab', 'Abm', 'A', 'Am', 'A#', 'A#m', 'Bb', 'Bbm', 'B', 'Bm', 'C', 'Cm', 'C#', 'C#m', 'Db', 'Dbm', 
+          'D', 'Dm', 'D#', 'D#m', 'Eb', 'Ebm', 'E', 'Em', 'F', 'Fm', 'F#', 'F#m', 'Gb', 'Gbm', 'G', 'Gm', 
+          'G#', 'G#m']
+
+keys = ['Ab', 'A', 'A#', 'Bb', 'B', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'G#']
+
+root = tk.Tk()
 root.title("Chord Transposer")
 root.geometry('1000x600')
 
-lbl = Label(root, text="Time Signature")
-lbl.grid()
+lbl_timeSig = ttk.Label(root, text="Time Signature")
+lbl_timeSig.grid(column=0, row=0)
 
-timeSigs = Combobox(root)
-timeSigs['values'] = ('4/4', '6/8')
-timeSigs['state'] = 'readonly'
+lbl_key = ttk.Label(root, text="Key")
+lbl_key.grid(column=0, row=1)
+
+lbl_measure1 = ttk.Label(root, text="Measure 1")
+lbl_measure1.grid(column=0, row=2)
+
+
+timeSig = tk.StringVar()
+timeSigs = ttk.Combobox(root, values=('4/4', '6/8'), state='readonly', width=4, textvariable=timeSig)
 timeSigs.grid(column=1, row=0)
+key = tk.StringVar()
+keys = ttk.Combobox(root, values=keys, state='readonly', width=4, textvariable=key)
+keys.grid(column=1,row=1)
+
+m1b1_chord = tk.StringVar()
+m1b1 = ttk.Combobox(root, values=chords, state='readonly', width=4, textvariable=m1b1_chord)
+m1b1.grid(column=1,row=2)
+m1b2_chord = tk.StringVar()
+m1b2 = ttk.Combobox(root, values=chords, state='readonly', width=4, textvariable=m1b2_chord)
+m1b2.grid(column=2,row=2)
+m1b3_chord = tk.StringVar()
+m1b3 = ttk.Combobox(root, values=chords, state='readonly', width=4, textvariable=m1b3_chord)
+m1b3.grid(column=3,row=2)
+m1b4_chord = tk.StringVar()
+m1b4 = ttk.Combobox(root, values=chords, state='readonly', width=4, textvariable=m1b4_chord)
+m1b4.grid(column=4,row=2)
+
+
+
+def clicked():
+    cpStr = m1b1_chord.get() + "|" + m1b2_chord.get() + "|" + m1b3_chord.get() + "|" + m1b4_chord.get()
+    cp = ChordProgression(cpStr, timeSig.get(), key.get())
+    showinfo(title="New Chord Progression", message=cp.ToString())
+
+showChords = ttk.Label(root, text="Chord Progression")
+showChords.grid(column=0, row=4)
+
+saveButton = ttk.Button(root, text="Save", width=50, command=clicked)
+saveButton.grid(column=0, row=3)
+
+
 
 root.mainloop()
