448fb0089f6a275f4ac424fbdc8fe6a35c094b52
connerng <cjngadisastra@gmail.com>
2024-07-09 22:19:37 -0700

    Changed create new screen to use pack instead of place

--- main.py
+++ main.py
@@ -22,45 +22,55 @@ def hide(widget):
 def create_new_clicked():
 
     def ts_44_clicked():
-        create_new_clicked()
         global timeSignature
         timeSignature = "4/4"
         global chordProgCB
         chordProgCB = []
         for r in range(1, 5):
+            curFrame = tk.Frame(root, bg='light yellow')
+            curFrame.pack(pady=3)
             for col in range(1, 5):
-                cur = ttk.Combobox(root, values=chords, state='readonly', width=5)
+                cur = ttk.Combobox(curFrame, values=chords, state='readonly', width=5)
                 cur.current(0)
-                cur.place(x = (col * 50) + 100, y = (r * 25) + 60)
+                cur.pack(side='left', padx=1)
                 chordProgCB.append(cur)
-    
+        saveButton.pack(pady=5)
+        warningLabel.pack(pady=5)
 
     def ts_68_clicked():
-        create_new_clicked()
         global timeSignature
         timeSignature = "6/8"
         global chordProgCB
         chordProgCB = []
         for r in range(1, 5):
+            curFrame = tk.Frame(root, bg='light yellow')
+            curFrame.pack(pady=3)
             for col in range(1, 7):
-                cur = ttk.Combobox(root, values=chords, state='readonly', width=5)
+                cur = ttk.Combobox(curFrame, values=chords, state='readonly', width=5)
                 cur.current(0)
-                cur.place(x = (col * 50) + 50, y = (r * 25) + 60)
+                cur.pack(side='left', padx=1)
                 chordProgCB.append(cur)
+        saveButton.pack(pady=5)
+        warningLabel.pack(pady=5)
 
     def save_clicked():
-        cpStr = "|"
-        for i in range(0, len(chordProgCB)):
-            cpStr += chordProgCB[i].get()
-            if timeSignature == "4/4":
-                if (i+1)%4 == 0:
-                    cpStr += "|"
-            else:
-                if (i+1)%6 == 0:
-                    cpStr += "|"
-        cp = ChordProgression(cp_name_entry.get(), cpStr, timeSignature, keysCB.get(), bpm_scale.get())
-        AddNew(cp)
-        lib_clicked()
+        if cp_name_entry.get() == "":
+            warningLabel.config(text="Please Enter Name")
+        elif keysCB.get() == '.':
+            warningLabel.config(text="Please Select Key")
+        else:
+            cpStr = "|"
+            for i in range(0, len(chordProgCB)):
+                cpStr += chordProgCB[i].get()
+                if timeSignature == "4/4":
+                    if (i+1)%4 == 0:
+                        cpStr += "|"
+                else:
+                    if (i+1)%6 == 0:
+                        cpStr += "|"
+            cp = ChordProgression(cp_name_entry.get(), cpStr, timeSignature, keysCB.get(), bpm_scale.get())
+            AddNew(cp)
+            lib_clicked()
     
     def bpm_down_clicked():
         bpm_scale.set(bpm_scale.get() - 1)
@@ -73,41 +83,42 @@ def create_new_clicked():
     new_cp_title = ttk.Label(root, text="Create New Chord Progression", font=('Helvetica', 14), background='light yellow')
     new_cp_title.pack()
 
-    lbl_timeSig = ttk.Label(root, text="Time Signature:", font=('Helvetica', 10), background='light yellow')
-    lbl_timeSig.pack()
+    nameFrame = tk.Frame(root, bg='light yellow')
+    nameFrame.pack(pady=5)
+    lbl_cpname = ttk.Label(nameFrame, text="Name: ", font=('Helvetica', 10), background='light yellow')
+    lbl_cpname.pack(side='left', padx=(0,2))
+    cp_name_entry = ttk.Entry(nameFrame)
+    cp_name_entry.pack(side='left', padx=2)
 
-    ts_44_button = ttk.Button(root, text="4/4", width=10, command=ts_44_clicked)
-    ts_44_button.place(x=180, y=50)
-
-    ts_68_button = ttk.Button(root, text="6/8", width=10, command=ts_68_clicked)
-    ts_68_button.place(x=250, y=50)
-
-    lbl_key = ttk.Label(root, text="Key: ", font=('Helvetica', 10), background='light yellow')
-    lbl_key.place(x=150, y=190)
-
-    keysCB = ttk.Combobox(root, values=keysList, state='readonly', width=5)
+    keyFrame = tk.Frame(root, bg='light yellow')
+    keyFrame.pack(pady=5)
+    lbl_key = ttk.Label(keyFrame, text="Key: ", font=('Helvetica', 10), background='light yellow')
+    lbl_key.pack(side='left', padx=(0,2))
+    keysCB = ttk.Combobox(keyFrame, values=keysList, state='readonly', width=5)
     keysCB.current(0)
-    keysCB.place(x=200, y=190)
+    keysCB.pack(side='left', padx=2)
 
-    lbl_cpname = ttk.Label(root, text="Name: ", font=('Helvetica', 10), background='light yellow')
-    lbl_cpname.place(x=150, y=215)
-
-    cp_name_entry = ttk.Entry(root)
-    cp_name_entry.place(x=200, y=215)
-
-    bpm_down = ttk.Button(root, text="<<", width=3, command=bpm_down_clicked)
-    bpm_down.place(x=70,y=280)
-
-    bpm_up = ttk.Button(root, text=">>", width=3, command=bpm_up_clicked)
-    bpm_up.place(x=408,y=280)
-    
-    bpm_scale = tk.Scale(root, from_=30, to=250, orient='horizontal', length=300, label="BPM")
+    bpmFrame = tk.Frame(root, bg='light yellow')
+    bpmFrame.pack(pady=5)
+    bpm_down = ttk.Button(bpmFrame, text="<<", width=3, command=bpm_down_clicked)
+    bpm_down.pack(side='left', padx=(0,2))
+    bpm_scale = tk.Scale(bpmFrame, from_=30, to=250, orient='horizontal', length=300, label="BPM")
     bpm_scale.set(100)
-    bpm_scale.place(x=100, y=250)
+    bpm_scale.pack(side='left', padx=2)
+    bpm_up = ttk.Button(bpmFrame, text=">>", width=3, command=bpm_up_clicked)
+    bpm_up.pack(side='left', padx=2)
 
+    timeSigFrame = tk.Frame(root, bg='light yellow')
+    timeSigFrame.pack(pady=5)
+    lbl_timeSig = ttk.Label(timeSigFrame, text="Time Signature:", font=('Helvetica', 10), background='light yellow')
+    lbl_timeSig.pack(side='left', padx=(0,2))
+    ts_44_button = ttk.Button(timeSigFrame, text="4/4", width=10, command=ts_44_clicked)
+    ts_44_button.pack(side='left', padx=2)
+    ts_68_button = ttk.Button(timeSigFrame, text="6/8", width=10, command=ts_68_clicked)
+    ts_68_button.pack(side='left', padx=2)
 
     saveButton = ttk.Button(root, text="Save", width=10, command=save_clicked)
-    saveButton.place(x=215, y=320)
+    warningLabel = ttk.Label(root, text="", font=('Helvetica', 10), background='light yellow')
 
 
 def play_clicked():
